5adba310e9044c651b819d707782a0276b301b3c
Joseph Cramsey <josephcsoftware@gmail.com>
2025-11-26 16:24:12 -0500

    sigterm handling

--- index.js
+++ index.js
@@ -1,19 +1,44 @@
 'use strict';
 
-// Web service
-(async () => {
-    const express = require('express');
-    const app = express();
-    const port = 3000;
-    const path = require('path');
+var needsToExit = false;
 
-    app.use(express.static(path.join(__dirname, 'public')));
-    app.use('/node_modules/@xterm/xterm', express.static(path.join(__dirname, 'node_modules/@xterm/xterm')));
+function throwExit() {
+    console.log("Throwing exit signal...");
+    needsToExit = true;
+}
 
-    app.listen(port, () => {
-        console.log(`Slime mold front-end ready at http://localhost:${port}/`);
+var signals = {
+    'SIGHUP': 1,
+    'SIGINT': 2,
+    'SIGTERM': 15
+};
+
+Object.keys(signals).forEach((signal) => {
+    process.on(signal, () => {
+        console.log(`Server heard ${signal} signal!`);
+        throwExit();
     });
-})();
+});
+
+try {
+    // Web service
+    (async () => {
+        const express = require('express');
+        const app = express();
+        const port = 3000;
+        const path = require('path');
+
+        app.use(express.static(path.join(__dirname, 'public')));
+        app.use('/node_modules/@xterm/xterm', express.static(path.join(__dirname, 'node_modules/@xterm/xterm')));
+
+        app.listen(port, () => {
+            console.log(`Slime mold front-end ready at http://localhost:${port}/`);
+        });
+    })();
+} catch(err) {
+    console.error(err);
+    throwExit();
+}
 
 // App
 (async () => {
@@ -22,6 +47,23 @@
 
     const client = new Client()
     await client.connect()
-    console.log("Hello world!");
+    console.log("Successfully connected to database!");
+
+    await client.query(`
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY,
+    mass DECIMAL(6,2),
+    saturation DECIMAL(5,2),
+    activity INTEGER
+);
+`.trim());
+
+    // Await some exit condition
+    while (!needsToExit) {
+        await new Promise(resolve => setTimeout(resolve, 500));
+    }
+
+    console.log("Closing node...");
     await client.end();
+    process.exit(0);
 })();
